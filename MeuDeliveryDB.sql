293bb41a06338b931e246e59641881b09ccf1bed
Rayara <rayara.almeida@ufrpe.br>
2026-09-13 18:10:04 -0300

    Atualiza backup do banco com WEB 02

--- MeuDeliveryDB.sql
+++ MeuDeliveryDB.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict uL0deFvQwgOcz2GKPs0y4nwVEeXeV1fgyWAe5LK3dW4tggvgGWm7WftUO4p1gvm
+\restrict IBgepoBBkHfqeHMKQbgxyYk2lCIvVlvQh3Sc0ePZdNRyBnBe5tPLDpc2aVn8MsS
 
 -- Dumped from database version 18.3
 -- Dumped by pg_dump version 18.3
 
--- Started on 2026-09-13 16:13:30
+-- Started on 2026-09-13 18:03:48
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -24,6 +24,133 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- TOC entry 228 (class 1259 OID 17867)
+-- Name: forma_pagamento_loja; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.forma_pagamento_loja (
+    id_forma_pagamento integer NOT NULL,
+    id_loja integer NOT NULL,
+    forma_pagamento character varying(30) NOT NULL,
+    CONSTRAINT chk_forma_pagamento CHECK (((forma_pagamento)::text = ANY ((ARRAY['dinheiro'::character varying, 'pix'::character varying, 'cartao_credito'::character varying, 'cartao_debito'::character varying])::text[])))
+);
+
+
+ALTER TABLE public.forma_pagamento_loja OWNER TO postgres;
+
+--
+-- TOC entry 227 (class 1259 OID 17866)
+-- Name: forma_pagamento_loja_id_forma_pagamento_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.forma_pagamento_loja_id_forma_pagamento_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.forma_pagamento_loja_id_forma_pagamento_seq OWNER TO postgres;
+
+--
+-- TOC entry 5064 (class 0 OID 0)
+-- Dependencies: 227
+-- Name: forma_pagamento_loja_id_forma_pagamento_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.forma_pagamento_loja_id_forma_pagamento_seq OWNED BY public.forma_pagamento_loja.id_forma_pagamento;
+
+
+--
+-- TOC entry 226 (class 1259 OID 17849)
+-- Name: horario_funcionamento; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.horario_funcionamento (
+    id_horario integer NOT NULL,
+    id_loja integer NOT NULL,
+    dia_semana character varying(15) NOT NULL,
+    hora_abertura time without time zone NOT NULL,
+    hora_fechamento time without time zone NOT NULL,
+    CONSTRAINT chk_dia_semana CHECK (((dia_semana)::text = ANY ((ARRAY['segunda'::character varying, 'terça'::character varying, 'quarta'::character varying, 'quinta'::character varying, 'sexta'::character varying, 'sábado'::character varying, 'domingo'::character varying])::text[])))
+);
+
+
+ALTER TABLE public.horario_funcionamento OWNER TO postgres;
+
+--
+-- TOC entry 225 (class 1259 OID 17848)
+-- Name: horario_funcionamento_id_horario_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.horario_funcionamento_id_horario_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.horario_funcionamento_id_horario_seq OWNER TO postgres;
+
+--
+-- TOC entry 5065 (class 0 OID 0)
+-- Dependencies: 225
+-- Name: horario_funcionamento_id_horario_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.horario_funcionamento_id_horario_seq OWNED BY public.horario_funcionamento.id_horario;
+
+
+--
+-- TOC entry 224 (class 1259 OID 17826)
+-- Name: loja; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.loja (
+    id_loja integer NOT NULL,
+    id_usuario integer NOT NULL,
+    nome character varying(100) NOT NULL,
+    foto character varying(500),
+    categoria character varying(30) NOT NULL,
+    endereco character varying(200) NOT NULL,
+    telefone character varying(20) NOT NULL,
+    aberta boolean DEFAULT false NOT NULL,
+    CONSTRAINT chk_categoria CHECK (((categoria)::text = ANY ((ARRAY['lanches'::character varying, 'pizzaria'::character varying, 'açaí'::character varying, 'mercado'::character varying])::text[])))
+);
+
+
+ALTER TABLE public.loja OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 17825)
+-- Name: loja_id_loja_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.loja_id_loja_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.loja_id_loja_seq OWNER TO postgres;
+
+--
+-- TOC entry 5066 (class 0 OID 0)
+-- Dependencies: 223
+-- Name: loja_id_loja_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.loja_id_loja_seq OWNED BY public.loja.id_loja;
+
 
 --
 -- TOC entry 220 (class 1259 OID 17795)
@@ -55,7 +182,7 @@ CREATE SEQUENCE public.perfil_id_perfil_seq
 ALTER SEQUENCE public.perfil_id_perfil_seq OWNER TO postgres;
 
 --
--- TOC entry 5027 (class 0 OID 0)
+-- TOC entry 5067 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: perfil_id_perfil_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -97,7 +224,7 @@ CREATE SEQUENCE public.usuario_id_usuario_seq
 ALTER SEQUENCE public.usuario_id_usuario_seq OWNER TO postgres;
 
 --
--- TOC entry 5028 (class 0 OID 0)
+-- TOC entry 5068 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: usuario_id_usuario_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -106,7 +233,31 @@ ALTER SEQUENCE public.usuario_id_usuario_seq OWNED BY public.usuario.id_usuario;
 
 
 --
--- TOC entry 4861 (class 2604 OID 17798)
+-- TOC entry 4881 (class 2604 OID 17870)
+-- Name: forma_pagamento_loja id_forma_pagamento; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.forma_pagamento_loja ALTER COLUMN id_forma_pagamento SET DEFAULT nextval('public.forma_pagamento_loja_id_forma_pagamento_seq'::regclass);
+
+
+--
+-- TOC entry 4880 (class 2604 OID 17852)
+-- Name: horario_funcionamento id_horario; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.horario_funcionamento ALTER COLUMN id_horario SET DEFAULT nextval('public.horario_funcionamento_id_horario_seq'::regclass);
+
+
+--
+-- TOC entry 4878 (class 2604 OID 17829)
+-- Name: loja id_loja; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.loja ALTER COLUMN id_loja SET DEFAULT nextval('public.loja_id_loja_seq'::regclass);
+
+
+--
+-- TOC entry 4876 (class 2604 OID 17798)
 -- Name: perfil id_perfil; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -114,7 +265,7 @@ ALTER TABLE ONLY public.perfil ALTER COLUMN id_perfil SET DEFAULT nextval('publi
 
 
 --
--- TOC entry 4862 (class 2604 OID 17809)
+-- TOC entry 4877 (class 2604 OID 17809)
 -- Name: usuario id_usuario; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -122,7 +273,43 @@ ALTER TABLE ONLY public.usuario ALTER COLUMN id_usuario SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 5019 (class 0 OID 17795)
+-- TOC entry 5058 (class 0 OID 17867)
+-- Dependencies: 228
+-- Data for Name: forma_pagamento_loja; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.forma_pagamento_loja (id_forma_pagamento, id_loja, forma_pagamento) FROM stdin;
+\.
+
+
+--
+-- TOC entry 5056 (class 0 OID 17849)
+-- Dependencies: 226
+-- Data for Name: horario_funcionamento; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.horario_funcionamento (id_horario, id_loja, dia_semana, hora_abertura, hora_fechamento) FROM stdin;
+1	1	segunda	08:00:00	18:00:00
+2	1	terça	08:00:00	18:00:00
+3	1	quarta	08:00:00	18:00:00
+4	1	quinta	08:00:00	18:00:00
+5	1	sexta	08:00:00	18:00:00
+\.
+
+
+--
+-- TOC entry 5054 (class 0 OID 17826)
+-- Dependencies: 224
+-- Data for Name: loja; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.loja (id_loja, id_usuario, nome, foto, categoria, endereco, telefone, aberta) FROM stdin;
+1	5	Loja Teste	https://exemplo.com/foto-loja.jpg	lanches	Rua Principal, 100	87977777777	f
+\.
+
+
+--
+-- TOC entry 5050 (class 0 OID 17795)
 -- Dependencies: 220
 -- Data for Name: perfil; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -136,7 +323,7 @@ COPY public.perfil (id_perfil, nome) FROM stdin;
 
 
 --
--- TOC entry 5021 (class 0 OID 17806)
+-- TOC entry 5052 (class 0 OID 17806)
 -- Dependencies: 222
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -146,7 +333,34 @@ COPY public.usuario (id_usuario, nome, email, telefone, senha, id_perfil) FROM s
 
 
 --
--- TOC entry 5029 (class 0 OID 0)
+-- TOC entry 5069 (class 0 OID 0)
+-- Dependencies: 227
+-- Name: forma_pagamento_loja_id_forma_pagamento_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.forma_pagamento_loja_id_forma_pagamento_seq', 1, false);
+
+
+--
+-- TOC entry 5070 (class 0 OID 0)
+-- Dependencies: 225
+-- Name: horario_funcionamento_id_horario_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.horario_funcionamento_id_horario_seq', 5, true);
+
+
+--
+-- TOC entry 5071 (class 0 OID 0)
+-- Dependencies: 223
+-- Name: loja_id_loja_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.loja_id_loja_seq', 1, true);
+
+
+--
+-- TOC entry 5072 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: perfil_id_perfil_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -155,7 +369,7 @@ SELECT pg_catalog.setval('public.perfil_id_perfil_seq', 4, true);
 
 
 --
--- TOC entry 5030 (class 0 OID 0)
+-- TOC entry 5073 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: usuario_id_usuario_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -164,7 +378,34 @@ SELECT pg_catalog.setval('public.usuario_id_usuario_seq', 6, true);
 
 
 --
--- TOC entry 4864 (class 2606 OID 17804)
+-- TOC entry 4897 (class 2606 OID 17876)
+-- Name: forma_pagamento_loja forma_pagamento_loja_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.forma_pagamento_loja
+    ADD CONSTRAINT forma_pagamento_loja_pkey PRIMARY KEY (id_forma_pagamento);
+
+
+--
+-- TOC entry 4895 (class 2606 OID 17860)
+-- Name: horario_funcionamento horario_funcionamento_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.horario_funcionamento
+    ADD CONSTRAINT horario_funcionamento_pkey PRIMARY KEY (id_horario);
+
+
+--
+-- TOC entry 4893 (class 2606 OID 17842)
+-- Name: loja loja_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.loja
+    ADD CONSTRAINT loja_pkey PRIMARY KEY (id_loja);
+
+
+--
+-- TOC entry 4886 (class 2606 OID 17804)
 -- Name: perfil perfil_nome_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -173,7 +414,7 @@ ALTER TABLE ONLY public.perfil
 
 
 --
--- TOC entry 4866 (class 2606 OID 17802)
+-- TOC entry 4888 (class 2606 OID 17802)
 -- Name: perfil perfil_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -182,7 +423,7 @@ ALTER TABLE ONLY public.perfil
 
 
 --
--- TOC entry 4869 (class 2606 OID 17817)
+-- TOC entry 4891 (class 2606 OID 17817)
 -- Name: usuario usuario_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -191,7 +432,7 @@ ALTER TABLE ONLY public.usuario
 
 
 --
--- TOC entry 4867 (class 1259 OID 17823)
+-- TOC entry 4889 (class 1259 OID 17823)
 -- Name: uq_usuario_email; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -199,7 +440,34 @@ CREATE UNIQUE INDEX uq_usuario_email ON public.usuario USING btree (lower((email
 
 
 --
--- TOC entry 4870 (class 2606 OID 17818)
+-- TOC entry 4901 (class 2606 OID 17877)
+-- Name: forma_pagamento_loja fk_forma_pagamento_loja; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.forma_pagamento_loja
+    ADD CONSTRAINT fk_forma_pagamento_loja FOREIGN KEY (id_loja) REFERENCES public.loja(id_loja);
+
+
+--
+-- TOC entry 4900 (class 2606 OID 17861)
+-- Name: horario_funcionamento fk_horario_loja; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.horario_funcionamento
+    ADD CONSTRAINT fk_horario_loja FOREIGN KEY (id_loja) REFERENCES public.loja(id_loja);
+
+
+--
+-- TOC entry 4899 (class 2606 OID 17843)
+-- Name: loja fk_loja_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.loja
+    ADD CONSTRAINT fk_loja_usuario FOREIGN KEY (id_usuario) REFERENCES public.usuario(id_usuario);
+
+
+--
+-- TOC entry 4898 (class 2606 OID 17818)
 -- Name: usuario fk_usuario_perfil; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -207,11 +475,11 @@ ALTER TABLE ONLY public.usuario
     ADD CONSTRAINT fk_usuario_perfil FOREIGN KEY (id_perfil) REFERENCES public.perfil(id_perfil);
 
 
--- Completed on 2026-09-13 16:13:30
+-- Completed on 2026-09-13 18:03:49
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict uL0deFvQwgOcz2GKPs0y4nwVEeXeV1fgyWAe5LK3dW4tggvgGWm7WftUO4p1gvm
+\unrestrict IBgepoBBkHfqeHMKQbgxyYk2lCIvVlvQh3Sc0ePZdNRyBnBe5tPLDpc2aVn8MsS
 
